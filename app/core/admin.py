7829d054aa5773bfddd0f7eee5bff819464527b3
basON <wch.basinski@gmail.com>
2023-06-30 18:30:43 +0200

    Add basic documentation

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+"""
+Django admin customizations
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """User admin"""
+
+    ordering = ['id']
+    list_display = ['email','name']
+    fieldsets = (
+        (None,{'fields':('email','password')}),
+        (_('Permissions')
+         ,{'fields':
+           ('is_active',
+            'is_staff',
+            'is_superuser')
+           }
+         ),
+        (_('Important dates'),{'fields':('last_login',)})
+    )
+    readonly_fields = ('last_login',)
+
+    add_fieldsets=(
+        (None,{
+            'classes':('wide',),
+            'fields':('email','password1','password2','name','is_staff','is_superuser','is_active',)
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,18 @@
+"""
+Test user api
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    """Helper function to create new user"""
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
